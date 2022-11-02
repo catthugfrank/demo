4590b023d264f6efe189e470b95d5e8443e2ceb0
Frank Vu <frankvu25@gmail.com>
2022-11-01 20:59:57 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Contact.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+public class Contact {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    private String name;
+    private String phone;
+
+
+}
